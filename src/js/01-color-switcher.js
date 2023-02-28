8685243500819eb37f29cceb62c0f177088cc0a4
Roman Petrenko <rpetrenko0105@gmail.com>
2023-02-28 17:01:55 +0200

    add script for HT 1

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,30 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const startEl = document.querySelector('[data-start]');
+const stopEl = document.querySelector('[data-stop]');
+const bodyEl = document.querySelector('body');
+// console.log(bodyEl);
+
+startEl.addEventListener('click', onStartBtnClick);
+stopEl.addEventListener('click', onStopBtnClick);
+
+// let intervalId = null;
+
+//* Функція запуску
+function onStartBtnClick() {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+
+  intervalId = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  console.log('Функція запуску зміни кольору');
+}
+
+//* Функція зупинки
+function onStopBtnClick() {
+  clearInterval(intervalId);
+
+  console.log('Зупинка кольору');
+}
